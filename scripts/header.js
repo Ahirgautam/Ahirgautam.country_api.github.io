48845f3660985d189c1a6c59ade68cafe6fe9eb8
Ahirgautam <100185475+Ahirgautam@users.noreply.github.com>
2025-04-17 21:09:08 +0530

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,20 +1,25 @@
-window.matchMedia("(prefers-color-scheme : dark)").addEventListener("change", (e)=>{
-    e.matches ? setToDark() : setToLight();
-});
-
 const theamToggler = document.getElementsByClassName("day_night_toggle")[0];
 const theamImg = document.querySelector(".day_night_toggle .toggle_icon");
 const theamText = document.querySelector(".day_night_toggle .toggle_text");
 
 
+
+if(window.matchMedia("(prefers-color-scheme : light)").matches || localStorage.getItem("theme") === "light"){
+    setToLight();
+}
+
 theamToggler.addEventListener("click", ()=>{
     if(document.body.dataset.theam === 'dark' ){
         setToLight();
+        localStorage.setItem("theme", "light");
     }else{
         setToDark();
+        localStorage.setItem("theme", "dark");
     }
 });
-
+window.matchMedia("(prefers-color-scheme : dark)").addEventListener("change", (e)=>{
+    e.matches ? setToDark() : setToLight();
+});
 
 function setToDark(){
     theamImg.src = "icons/moon.svg";
